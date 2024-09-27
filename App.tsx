3c07539cf4dbee5bd4fadb03841dc80a42dacb44
Victor Hugo Soligo <victorsoligo@gmail.com>
2024-09-27 15:59:35 -0300

    chore: nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My Goals!</Text>
+    <View className="flex-1 items-center justify-center bg-white-600">
+      <Text>My Goals</Text>
 
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
