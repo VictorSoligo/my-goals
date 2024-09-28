cb6534658931238416e95336aa3bc3130e7ebc9b
Victor Hugo Soligo <victorsoligo@gmail.com>
2024-09-28 16:50:06 -0300

    chore: gluestack setup

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import "./src/styles/global.css"
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
+import { Button } from '@/components/ui/button'
+import './src/styles/global.css'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>My Goals asdasd</Text>
+    <GluestackUIProvider>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Button variant="outline">
+          <Text>Login</Text>
+        </Button>
 
-      <StatusBar style="auto" />
-    </View>
-  );
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
+  )
 }
-
-
---
+++ src/components/ui/gluestack-ui-provider/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { config } from './config'
+import { ColorSchemeName, useColorScheme, View, ViewProps } from 'react-native'
+import { colorScheme as colorSchemeNW } from 'nativewind'
+
+type ModeType = 'light' | 'dark' | 'system'
+
+const getColorSchemeName = (
+  colorScheme: ColorSchemeName,
+  mode: ModeType,
+): 'light' | 'dark' => {
+  if (mode === 'system') {
+    return colorScheme ?? 'light'
+  }
+  return mode
+}
+
+export function GluestackUIProvider({
+  mode = 'light',
+  ...props
+}: {
+  mode?: 'light' | 'dark' | 'system'
+  children?: React.ReactNode
+  style?: ViewProps['style']
+}) {
+  const colorScheme = useColorScheme()
+
+  const colorSchemeName = getColorSchemeName(colorScheme, mode)
+
+  colorSchemeNW.set(mode)
+
+  return (
+    <View
+      style={[
+        config[colorSchemeName],
+        { flex: 1, height: '100%', width: '100%' },
+        props.style,
+      ]}
+    >
+      {props.children}
+    </View>
+  )
+}
