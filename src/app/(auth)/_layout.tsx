2f28946020d89179e126d1a2c410a21db89ab3aa
Victor Hugo Soligo <victorsoligo@gmail.com>
2024-10-06 20:31:15 -0300

    chore: tab bar style

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,13 +1,25 @@
 import { Tabs } from 'expo-router'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 export default function AuthLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveBackgroundColor: '#303956',
+        tabBarInactiveBackgroundColor: '#303956',
+        tabBarActiveTintColor: '#fff',
+        tabBarShowLabel: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Exercícios',
           headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="dumbbell" size={16} color={color} />
+          ),
         }}
       />
 
@@ -16,6 +28,9 @@ export default function AuthLayout() {
         options={{
           title: 'Sessões',
           headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="sports-martial-arts" size={20} color={color} />
+          ),
         }}
       />
     </Tabs>
